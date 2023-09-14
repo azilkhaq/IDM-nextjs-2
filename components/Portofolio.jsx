07b285ffc07c1fc79b174d07fbe3b934d3bdfbf4
gilang ramadani <95260173+rmdnigilang@users.noreply.github.com>
2023-09-14 15:08:11 +0700

    2

--- components/Portofolio.jsx
+++ components/Portofolio.jsx
@@ -8,7 +8,6 @@ const Portofolio = () => {
                 <h1 className="text-white text-3xl font-semibold mb-5">P O R T O F O L I O</h1>
                 <hr className="border-t-1 border-sky-600 w-full mx-auto" />
             </div>
-
             {/* Kolom Kiri (Teks) */}
             <div className="col-span-1 mt-18">
                 <h2 className="text-white text-2xl font-semibold">Standardize IoT Gateway using MQTT</h2>
@@ -20,7 +19,6 @@ const Portofolio = () => {
                     With switch trend from P to P SMS to become A to P SMS, this area growth and required more advance solution, in terms of service and agile delivery. Facebook, Whatapps and up to domestic Partner require an Agile SMS platform with high throughput and High continuity integration. We offer new models of SMPP method, with custom solution by adopt async process instead of normal way (sync process), this is give solution to overcome High latency issue
                 </p>
             </div>
-
             {/* Kolom Kanan (Gambar) */}
             <div className="col-span-1 flex justify-center items-center">
                 <img src="/portofolio.png" alt="Gambar" className="w-auto h-auto" />
